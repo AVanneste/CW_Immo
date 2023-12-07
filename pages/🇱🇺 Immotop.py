9cf54cce0c7cf6936fde0bda76883142776f541a
Antoine <113119888+gastoontje@users.noreply.github.com>
2023-12-07 11:59:37 +0100

    reset homepage file name

--- pages/🇱🇺 Immotop.py
+++ pages/🇱🇺 Immotop.py
@@ -10,6 +10,8 @@ from scripts.create_map import create_map_immotop
 import folium
 from streamlit_folium import folium_static
 
+#TO DO: clean columns in results dfs
+
 # Home page display
 st.set_page_config(layout="wide", page_title="Immotop Scraper", page_icon = 'data/CW_icon.png')
 
