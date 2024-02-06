00ad996e1248ff48bbf26480cb4e84680e32529c
Antoine <113119888+gastoontje@users.noreply.github.com>
2024-02-06 11:10:09 +0100

    lu maintenance

--- pages/🇱🇺 Immotop.py
+++ pages/🇱🇺 Immotop.py
@@ -23,6 +23,7 @@ with col1:
     st.write(' ')
 with col2:
     st.image('data/CW_banner1.png', width=600)
+    st.image('data/under_maint.png', width=600)
 with col3:
     st.write(' ')
 
@@ -30,93 +31,93 @@ with col3:
 st.title('Immotop Scraper')
 
 #data selection and filters
-with open('data/search_data_immotop.json') as json_file:
-    search_data = json.load(json_file)
+# with open('data/search_data_immotop.json') as json_file:
+#     search_data = json.load(json_file)
 
-#select boxes and display
-sale_rent = st.selectbox('Sale or Rent?', ['Sale', 'Rent'], index=None, placeholder='Select For Sale or For Rent option')
+# #select boxes and display
+# sale_rent = st.selectbox('Sale or Rent?', ['Sale', 'Rent'], index=None, placeholder='Select For Sale or For Rent option')
 
-if sale_rent:
-    if sale_rent == 'Rent':
-        property_type = st.multiselect('Which type(s) of property for rent are you looking for?', search_data['property_types_rent'].keys(), placeholder='Select multiple, leave empty if you want everything')
-        property_type = [search_data['property_types_rent'][x] for x in property_type]
-    else:
-        property_type = st.multiselect('Which type(s) of property for sale are you looking for?', search_data['property_types_sale'].keys(), placeholder='Select multiple, leave empty if you want everything')
-        property_type = [search_data['property_types_sale'][x] for x in property_type]
+# if sale_rent:
+#     if sale_rent == 'Rent':
+#         property_type = st.multiselect('Which type(s) of property for rent are you looking for?', search_data['property_types_rent'].keys(), placeholder='Select multiple, leave empty if you want everything')
+#         property_type = [search_data['property_types_rent'][x] for x in property_type]
+#     else:
+#         property_type = st.multiselect('Which type(s) of property for sale are you looking for?', search_data['property_types_sale'].keys(), placeholder='Select multiple, leave empty if you want everything')
+#         property_type = [search_data['property_types_sale'][x] for x in property_type]
     
-    st.markdown('Where do you want to search?')
-    zone = st.selectbox('Zone', options=list(search_data['zones'].keys()))
-    zone = search_data['zones'][zone]
+#     st.markdown('Where do you want to search?')
+#     zone = st.selectbox('Zone', options=list(search_data['zones'].keys()))
+#     zone = search_data['zones'][zone]
     
-    if not property_type:
-        if sale_rent == 'Rent':
-            property_type = list(search_data['property_types_rent'].values())
-        else:
-            property_type = list(search_data['property_types_sale'].values())
+#     if not property_type:
+#         if sale_rent == 'Rent':
+#             property_type = list(search_data['property_types_rent'].values())
+#         else:
+#             property_type = list(search_data['property_types_sale'].values())
 
-    # Process and save results
-    if st.button('Search'):
+#     # Process and save results
+#     if st.button('Search'):
 
-        with st.spinner('Wait for it...'):
+#         with st.spinner('Wait for it...'):
 
-            result = scripts.immotop_scraper.run(search_data['sale_rent'][sale_rent], property_type, zone)
-            if result.empty:
-                st.markdown('No results found')
-            else:
-                result = transform_immotop(result)
+#             result = scripts.immotop_scraper.run(search_data['sale_rent'][sale_rent], property_type, zone)
+#             if result.empty:
+#                 st.markdown('No results found')
+#             else:
+#                 result = transform_immotop(result)
 
-                st.dataframe(result, column_config={"url": st.column_config.LinkColumn("url")})
+#                 st.dataframe(result, column_config={"url": st.column_config.LinkColumn("url")})
                 
-                output = BytesIO()
-                with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-                    result.to_excel(writer, sheet_name='Results', index=False)
+#                 output = BytesIO()
+#                 with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+#                     result.to_excel(writer, sheet_name='Results', index=False)
                     
-                st.download_button(label='📥 Download Search Results',
-                                                    data=output ,
-                                                    file_name= 'immotop.xlsx',
-                                                    mime="application/vnd.ms-excel")
+#                 st.download_button(label='📥 Download Search Results',
+#                                                     data=output ,
+#                                                     file_name= 'immotop.xlsx',
+#                                                     mime="application/vnd.ms-excel")
 
-                with st.spinner('Map Loading...'):
+#                 with st.spinner('Map Loading...'):
 
-                    my_map = create_map_immotop(result)
+#                     my_map = create_map_immotop(result)
 
-                # map_show = st.checkbox('Show map')
-                # if map_show:
-                    try:
-                        folium_static(my_map, width=1500, height=800)
-                    except:
-                        st.markdown('No result to show on map')
+#                 # map_show = st.checkbox('Show map')
+#                 # if map_show:
+#                     try:
+#                         folium_static(my_map, width=1500, height=800)
+#                     except:
+#                         st.markdown('No result to show on map')
     
-    if st.button('Full Search (VERY SLOW)'):
+#     if st.button('Full Search (VERY SLOW)'):
 
-        with st.spinner('Wait for it...'):
+#         with st.spinner('Wait for it...'):
 
-            result = scripts.immotop_scraper_full.run(search_data['sale_rent'][sale_rent], property_type, zone)
-            if result.empty:
-                st.markdown('No results found')
-            else:
-                result = transform_immotop(result)
+#             result = scripts.immotop_scraper_full.run(search_data['sale_rent'][sale_rent], property_type, zone)
+#             if result.empty:
+#                 st.markdown('No results found')
+#             else:
+#                 result = transform_immotop(result)
                 
-                st.dataframe(result, column_config={"url": st.column_config.LinkColumn("url")})
+#                 st.dataframe(result, column_config={"url": st.column_config.LinkColumn("url")})
                 
-                output = BytesIO()
-                with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-                    result.to_excel(writer, sheet_name='Results', index=False)
+#                 output = BytesIO()
+#                 with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+#                     result.to_excel(writer, sheet_name='Results', index=False)
                     
-                st.download_button(label='📥 Download Full Search Results',
-                                                    data=output ,
-                                                    file_name= 'immotop_full.xlsx',
-                                                    mime="application/vnd.ms-excel")
+#                 st.download_button(label='📥 Download Full Search Results',
+#                                                     data=output ,
+#                                                     file_name= 'immotop_full.xlsx',
+#                                                     mime="application/vnd.ms-excel")
                 
-                with st.spinner('Map Loading...'):
-                    my_map = create_map_immotop(result)
+#                 with st.spinner('Map Loading...'):
+#                     my_map = create_map_immotop(result)
 
-                # map_show = st.checkbox('Show map')
-                # if map_show:
-                    try:
-                        folium_static(my_map, width=1500, height=800)
-                    except:
-                        st.markdown('No result to show on map')
+#                 # map_show = st.checkbox('Show map')
+#                 # if map_show:
+#                     try:
+#                         folium_static(my_map, width=1500, height=800)
+#                     except:
+#                         st.markdown('No result to show on map')
 
 # Hide the footer
 hide_default_format = """
--- pages/🇱🇺 atHome.py
+++ pages/🇱🇺 atHome.py
@@ -22,68 +22,69 @@ with col1:
     st.write(' ')
 with col2:
     st.image('data/CW_banner1.png', width=600)
+    st.image('data/under_maint.png', width=600)
 with col3:
     st.write(' ')
 
 # Title of the project
 st.title('atHome Scraper')
 
-#data selection and filters
-with open('data/search_data_athome.json') as json_file:
-    search_data = json.load(json_file)
+# #data selection and filters
+# with open('data/search_data_athome.json') as json_file:
+#     search_data = json.load(json_file)
 
-#select boxes and display
-sale_rent = st.selectbox('Sale or Rent?', ['Sale', 'Rent'], index=None, placeholder='Select For Sale or For Rent option')
+# #select boxes and display
+# sale_rent = st.selectbox('Sale or Rent?', ['Sale', 'Rent'], index=None, placeholder='Select For Sale or For Rent option')
 
-if sale_rent:
-    property_type_list = st.multiselect('Which type(s) of property are you looking for?', search_data['property_types'].keys(), placeholder='Select multiple, leave empty if you want to search everything')
-    property_type_list = [search_data['property_types'][x] for x in property_type_list]
-    if not property_type_list:
-        property_type_str = '' #list(search_data['property_types'].values())
-    else:
-        property_type_str = ','.join(property_type_list)
+# if sale_rent:
+#     property_type_list = st.multiselect('Which type(s) of property are you looking for?', search_data['property_types'].keys(), placeholder='Select multiple, leave empty if you want to search everything')
+#     property_type_list = [search_data['property_types'][x] for x in property_type_list]
+#     if not property_type_list:
+#         property_type_str = '' #list(search_data['property_types'].values())
+#     else:
+#         property_type_str = ','.join(property_type_list)
 
-    st.markdown('Where do you want to search?')
-    zones = st.multiselect('Zone', options=list(search_data['zones'].keys()), placeholder='Select multiple, leave empty if you want to search the whole country')
-    zones = [search_data['zones'][x] for x in zones]
+#     st.markdown('Where do you want to search?')
+#     zones = st.multiselect('Zone', options=list(search_data['zones'].keys()), placeholder='Select multiple, leave empty if you want to search the whole country')
+#     zones = [search_data['zones'][x] for x in zones]
     
-    if not zones:
-        zone_str = 'faee1a4a'
-    else:
-        zone_str = ','.join(zones)
+#     if not zones:
+#         zone_str = 'faee1a4a'
+#     else:
+#         zone_str = ','.join(zones)
 
-    # Process and save results
-    if st.button('Search'):
+#     # Process and save results
+#     if st.button('Search'):
 
-        with st.spinner('Wait for it...'):
+#         with st.spinner('Wait for it...'):
 
-            result = scripts.athome_scraper.run(search_data['sale_rent'][sale_rent], property_type_str, zone_str)
-            if result.empty:
-                st.markdown('No results found')
-            else:
-                result = transform_athome(result)
+#             result = scripts.athome_scraper.run(search_data['sale_rent'][sale_rent], property_type_str, zone_str)
+#             if result.empty:
+#                 st.markdown('No results found')
+#             else:
+#                 result = transform_athome(result)
 
-                st.dataframe(result, column_config={"url": st.column_config.LinkColumn("url")})
+#                 st.dataframe(result, column_config={"url": st.column_config.LinkColumn("url")})
                 
-                output = BytesIO()
-                with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-                    result.to_excel(writer, sheet_name='Results', index=False)
+#                 output = BytesIO()
+#                 with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+#                     result.to_excel(writer, sheet_name='Results', index=False)
                     
-                st.download_button(label='📥 Download Search Results',
-                                                    data=output ,
-                                                    file_name= 'athome.xlsx',
-                                                    mime="application/vnd.ms-excel")
+#                 st.download_button(label='📥 Download Search Results',
+#                                                     data=output ,
+#                                                     file_name= 'athome.xlsx',
+#                                                     mime="application/vnd.ms-excel")
 
-                with st.spinner('Map Loading...'):
+#                 with st.spinner('Map Loading...'):
 
-                    my_map = create_map_athome(result)
+#                     my_map = create_map_athome(result)
 
-                # map_show = st.checkbox('Show map')
-                # if map_show:
-                    try:
-                        folium_static(my_map, width=1500, height=800)
-                    except:
-                        st.markdown('No result to show on map')
+#                 # map_show = st.checkbox('Show map')
+#                 # if map_show:
+#                     try:
+#                         folium_static(my_map, width=1500, height=800)
+#                     except:
+#                         st.markdown('No result to show on map')
     
     # if st.button('Full Search (VERY SLOW)'):
 
