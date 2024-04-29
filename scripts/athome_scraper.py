be5b236237e7be6d761ead74d87156917c3b43fd
Antoine <113119888+gastoontje@users.noreply.github.com>
2024-04-29 16:58:17 +0200

    ath

--- scripts/athome_scraper.py
+++ scripts/athome_scraper.py
@@ -12,28 +12,28 @@ def get_data_from_search_results(i, rent_sale, property_type, zone, session):
         url = f'https://www.athome.lu/en/srp/?tr={rent_sale}&ptypes={property_type}&q={zone}&sort=date_desc&page={i}'
     r = session.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
-    try:
-        results = soup.select("script")[6].contents[0]
-        athome = json.loads(results[27:-1])
-        prop_list = []
-        for item in athome['search']['list']:
-            [item.pop(key) for key in ["media", "meta", "depth", "publisher", "config", "others"]]
-            if item['has_children']==True:
-                groupId = item['id']
-                item['project.id'] = groupId
-                prop_list.append(item)
-                for child in item['children']['data']:
-                    [child.pop(key) for key in ["media", "meta", "depth", "publisher", "config", "others"]]
-                    child['project.id'] = groupId
-                    prop_list.append(child)
-                    child['id'] = child['listing_id']
-                    del child['listing_id']
-            else:
-                prop_list.append(item)
-        return pd.json_normalize(prop_list)
-    except:
-        print("content not found")
-        None
+    # try:
+    results = soup.select("script")[6].contents[0]
+    athome = json.loads(results[27:-1])
+    prop_list = []
+    for item in athome['search']['list']:
+        [item.pop(key) for key in ["media", "meta", "depth", "publisher", "config", "others"]]
+        if item['has_children']==True:
+            groupId = item['id']
+            item['project.id'] = groupId
+            prop_list.append(item)
+            for child in item['children']['data']:
+                [child.pop(key) for key in ["media", "meta", "depth", "publisher", "config", "others"]]
+                child['project.id'] = groupId
+                prop_list.append(child)
+                child['id'] = child['listing_id']
+                del child['listing_id']
+        else:
+            prop_list.append(item)
+    return pd.json_normalize(prop_list)
+    # except:
+    #     print("content not found")
+    #     None
 
 def get_data_for_category(rent_sale, property_type, zone, session):
     if property_type == '':
